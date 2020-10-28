a7e54cf91d8bcb671b9fb5c876a38c1ea7ae11fd
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-10-28 23:08:02 +0100

    fixing menu burger

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,16 @@
-const menuBurger = document.querySelector('#menu_burger');
-const menuList = document.querySelector('#menu_list')
+const menuBurger = document.querySelector('#menuBurger');
+const menuClose = document.querySelector('#menuClose');
+const menuList = document.querySelector('#menuList');
+const menu = document.querySelector('.menu')
 
-menuBurger.addEventListener('click', ()=>{
-    menuBurger.className='hidden';
+menu.addEventListener('click', ()=>{
+    if(menuBurger.className === 'hidden'){
+        menuBurger.className = 'fa fa-bars visible';
+        menuClose.className = 'fa fa-close hidden';
+    }
+    if(menuClose.className==='hidden'){
+        menuBurger.className = 'fa fa-bars hidden';
+        menuClose.className = 'fa fa-close visible';
+    }
+    
 })
