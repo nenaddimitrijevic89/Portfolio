fdfb44939fcd24cad65fbc36d97519a45532fdea
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-10-29 20:08:31 +0100

    hide nav list

--- js/demo.js
+++ js/demo.js
@@ -1,15 +1,17 @@
 const menuBurger = document.querySelector('#menuBurger');
 const menuClose = document.querySelector('#menuClose');
 const menuList = document.querySelector('#menuList');
-const menu = document.querySelector('.menu')
+const menu = document.querySelector('#menu')
 
 menu.addEventListener('click', ()=>{
-    if(menuBurger.className === 'hidden'){
-        menuBurger.className = 'fa fa-bars visible';
-        menuClose.className = 'fa fa-close hidden';
+    if(menuBurger.className === 'fa fa-bars'){
+        menuBurger.className = 'fa fa-bars inactive';
+        menuClose.className = 'fa fa-close';
+        menuList.className='';
     }else{
-        menuBurger.className = 'fa fa-bars hidden';
-        menuClose.className = 'fa fa-close visible';
+        menuBurger.className = 'fa fa-bars';
+        menuClose.className = 'fa fa-close inactive';
+        menuList.className='hidden';
     }
     
 })
