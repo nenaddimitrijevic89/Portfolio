62f6e4e516b553e9ef7e1cb47ae6be6fb8ca6567
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-10-29 20:30:28 +0100

    main section

--- js/demo.js
+++ js/demo.js
@@ -4,14 +4,13 @@ const menuList = document.querySelector('#menuList');
 const menu = document.querySelector('#menu')
 
 menu.addEventListener('click', ()=>{
-    if(menuBurger.className === 'fa fa-bars'){
+    if(menuBurger.className === 'fa fa-bars') {
         menuBurger.className = 'fa fa-bars inactive';
         menuClose.className = 'fa fa-close';
         menuList.className='';
-    }else{
+    }else {
         menuBurger.className = 'fa fa-bars';
         menuClose.className = 'fa fa-close inactive';
         menuList.className='hidden';
-    }
-    
+    }    
 })
