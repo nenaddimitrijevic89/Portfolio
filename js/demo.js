672970a6de2a5e982680179a799f4bdb5f3a5f30
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-10-31 12:46:01 +0100

    adding jquery

--- js/demo.js
+++ js/demo.js
@@ -3,14 +3,18 @@ const menuClose = document.querySelector('#menuClose');
 const menuList = document.querySelector('#menuList');
 const menu = document.querySelector('#menu')
 
-menu.addEventListener('click', ()=>{
-    if(menuBurger.className === 'fa fa-bars') {
+menu.addEventListener('click', () => {
+    if (menuBurger.className === 'fa fa-bars') {
         menuBurger.className = 'fa fa-bars inactive';
         menuClose.className = 'fa fa-close';
-        menuList.className='';
-    }else {
+        menuList.className = '';
+    } else {
         menuBurger.className = 'fa fa-bars';
         menuClose.className = 'fa fa-close inactive';
-        menuList.className='hidden';
-    }    
+        menuList.className = 'hidden';
+    }
+})
+
+$('#menu').on('click', function() {
+    console.log('click');
 })
