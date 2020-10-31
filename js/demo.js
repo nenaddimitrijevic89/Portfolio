91a2a6acfb11f820b77c979513227dd5708ff882
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-10-31 14:07:12 +0100

    hidding menu

--- js/demo.js
+++ js/demo.js
@@ -1,20 +1,5 @@
-const menuBurger = document.querySelector('#menuBurger');
-const menuClose = document.querySelector('#menuClose');
-const menuList = document.querySelector('#menuList');
-const menu = document.querySelector('#menu')
-
-menu.addEventListener('click', () => {
-    if (menuBurger.className === 'fa fa-bars') {
-        menuBurger.className = 'fa fa-bars inactive';
-        menuClose.className = 'fa fa-close';
-        menuList.className = '';
-    } else {
-        menuBurger.className = 'fa fa-bars';
-        menuClose.className = 'fa fa-close inactive';
-        menuList.className = 'hidden';
-    }
-})
-
-$('#menu').on('click', function() {
-    console.log('click');
+$('#menu').click(() => {
+    $('#menuBurger').toggleClass('inactive')
+    $('#menuClose').toggleClass('inactive')
+    $('#menuList').toggleClass('hidden');
 })
