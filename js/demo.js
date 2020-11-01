7475b34972a33e4d0c2acdd339d6862f3680e807
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-11-01 20:51:42 +0100

    menu list fade

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,5 @@
 $('#menu').click(() => {
     $('#menuBurger').toggleClass('inactive')
     $('#menuClose').toggleClass('inactive')
-    $('#menuList').toggleClass('hidden');
+    $('#menuList').toggleClass('hidden').fadeOut(0).fadeIn(800);
 })
