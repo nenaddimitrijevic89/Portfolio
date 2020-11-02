79dbc6767ffe3af28228bb8bbcf6a115934470dc
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-11-02 22:29:13 +0100

    changing back color

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,5 @@
 $('#menu').click(() => {
     $('#menuBurger').toggleClass('inactive')
     $('#menuClose').toggleClass('inactive')
-    $('#menuList').toggleClass('hidden').fadeOut(0.1).fadeIn(800);
+    $('#menuList').toggleClass('hidden').hide().fadeIn(800);
 })
