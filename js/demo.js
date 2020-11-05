7376671e3088e4e16c64516664bc6fd442c388b8
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-11-05 22:19:57 +0100

    about section changing

--- js/demo.js
+++ js/demo.js
@@ -3,3 +3,36 @@ $('#menu').click(() => {
     $('#menuClose').toggleClass('inactive')
     $('#menuList').toggleClass('hidden').hide().fadeIn(800);
 })
+
+Array.prototype.myMap=function(call){
+    const newArr=[];
+    for(let i=0; i<this.length; i++){
+        newArr.push(call(this[i], i, this))
+    }
+    return newArr;
+}
+
+Array.prototype.myFilter=function(call){
+
+    const newArr=[]
+    for(let i=0; i<this.length; i++){
+        if(call(this[i])){
+            newArr.push(this[i])
+        }
+    }
+    return newArr;
+}
+
+const sample=[1, 2, 3];
+// const c=sample.filter(el=>el>1)
+// console.log(c)
+
+const b=sample.myFilter(el => el>1);
+
+console.log(b)
+
+const a=sample.myMap(el => el*2);
+
+console.log(a);
+
+
