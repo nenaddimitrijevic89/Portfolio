83ced55af46583d606440932a8d04543bfe468da
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-11-10 19:54:07 +0100

    adding text

--- js/demo.js
+++ js/demo.js
@@ -1,10 +1,10 @@
 $('#menu').click(() => {
-    $('#menuBurger').toggleClass('inactive')
-    $('#menuClose').toggleClass('inactive')
-    $('#menuList').toggleClass('hidden').hide().fadeIn(800);
+  $('#menuBurger').toggleClass('inactive')
+  $('#menuClose').toggleClass('inactive')
+  $('#menuList').toggleClass('hidden').hide().fadeIn(800);
 })
 
-let a = [1,2,3,4,5,6,7,9,10],
+let a = [1, 2, 3, 4, 5, 6, 9, 10],
   count = 10,
   missing = [];
 
