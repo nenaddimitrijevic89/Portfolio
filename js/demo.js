e48de53628305e6ab99f82d8efbfd08db0c1070d
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-11-17 18:28:33 +0100

    adding style

--- js/demo.js
+++ js/demo.js
@@ -6,8 +6,8 @@ $('#menu').click(() => {
 
 $('.project').hover(
   function () {
-    $('project h5').addClass('inactive')
-    $(this).find('h5').removeClass('inactive').addClass('absolute').hide().fadeIn(400)
+    $('.project h5').addClass('inactive')
+    $(this).find('h5').removeClass('inactive').addClass('absolute').hide().fadeIn(200)
   },
   function () {
     $(this).find("h5").hide().addClass('inactive').removeClass('absolute')
