284a5b002c2707e5041863b940b3df18eb2e74b2
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-12-12 13:15:49 +0100

    finished

--- js/demo.js
+++ js/demo.js
@@ -1,35 +1,35 @@
 $('#menu').click(() => {
-  $('#menuBurger').toggleClass('inactive')
-  $('#menuClose').toggleClass('inactive')
-  $('#menuList').toggleClass('hidden').hide().fadeIn(800);
+    $('#menuBurger').toggleClass('inactive')
+    $('#menuClose').toggleClass('inactive')
+    $('#menuList').toggleClass('hidden').hide().fadeIn(800);
 })
 
 $('.project').hover(
-  function () {
-    $('.project h5').addClass('inactive')
-    $(this).find('h5').removeClass('inactive').addClass('absolute').hide().fadeIn(200)
-  },
-  function () {
-    $(this).find("h5").hide().addClass('inactive').removeClass('absolute')
-  }
+    function() {
+        $('.project h5').addClass('inactive')
+        $(this).find('h5').removeClass('inactive').addClass('absolute').hide().fadeIn(200)
+    },
+    function() {
+        $(this).find("h5").hide().addClass('inactive').removeClass('absolute')
+    }
 )
 
 $('.contact__icon').hover(
-  function () {
-    $(this).addClass('animate__animated animate__heartBeat')
-  },
-  function () {
-    $(this).removeClass('animate__animated animate__heartBeat')
-  }
+    function() {
+        $(this).addClass('animate__animated animate__heartBeat')
+    },
+    function() {
+        $(this).removeClass('animate__animated animate__heartBeat')
+    }
 )
 
 $('.circle').hover(
-  function () {
-    $(this).removeClass('animate__jackInTheBox').addClass('animate__heartBeat')
-  },
-  function () {
-    $(this).removeClass('animate__heartBeat')
-  }
+    function() {
+        $(this).removeClass('animate__jackInTheBox').addClass('animate__heartBeat')
+    },
+    function() {
+        $(this).removeClass('animate__heartBeat')
+    }
 )
 
-setInterval((function () { $('#developer').fadeIn(2500).fadeOut(1000) }), 1000);
+setInterval((function() { $('#developer').fadeIn(2500).fadeOut(1000) }), 1000);
